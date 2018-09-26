8fd4880ff6231841bdda94560580126e538e8e7a
Jessie Beech <jessie@jessiebeech.com>
2018-09-26 14:20:47 +0100

    basic board layout

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,17 +5,17 @@ export default class Board extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div>
+                <div className="row">
                     <Slot value={this.props.slots.box1} />
                     <Slot value={this.props.slots.box2} />
                     <Slot value={this.props.slots.box3} />
                 </div>
-                <div>
+                <div className="row">
                     <Slot value={this.props.slots.box4} />
                     <Slot value={this.props.slots.box5} />
                     <Slot value={this.props.slots.box6} />
                 </div>
-                <div>
+                <div className="row">
                     <Slot value={this.props.slots.box7} />
                     <Slot value={this.props.slots.box8} />
                     <Slot value={this.props.slots.box9} />
--- src/components/Slot.js
+++ src/components/Slot.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default class Slot extends React.Component {
-    render() {
-        return <h2>{this.props.value}</h2>;
-    }
+  render() {
+    return (
+      <div className="slot">
+        <h2>{this.props.value}</h2>
+      </div>
+    );
+  }
 }
