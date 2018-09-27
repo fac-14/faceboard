814ed43a5f936b9f266f9db0cd1bc43713259ae3
martingaston <squidman@gmail.com>
2018-09-27 11:23:16 +0100

    Add CSS clip paths to map image onto Slot components Relates #3

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,57 +2,57 @@ import React from "react";
 import Slot from "./Slot";
 
 export default class Board extends React.Component {
-  render() {
-      const winning = this.props.complete
-    return (
-      <React.Fragment>
-         <h1>{winning ? "Ya Won" : "" }</h1> 
-         <button onClick={this.props.scramble}>Scramble</button>
-        <div>
-          <div className="row">
-            <Slot
-              value={this.props.board[0]}
-              userMakesMove={this.props.userMakesMove}
-            />
-            <Slot
-              value={this.props.board[1]}
-              userMakesMove={this.props.userMakesMove}
-            />
-            <Slot
-              value={this.props.board[2]}
-              userMakesMove={this.props.userMakesMove}
-            />
-          </div>
-          <div className="row">
-            <Slot
-              value={this.props.board[3]}
-              userMakesMove={this.props.userMakesMove}
-            />
-            <Slot
-              value={this.props.board[4]}
-              userMakesMove={this.props.userMakesMove}
-            />
-            <Slot
-              value={this.props.board[5]}
-              userMakesMove={this.props.userMakesMove}
-            />
-          </div>
-          <div className="row">
-            <Slot
-              value={this.props.board[6]}
-              userMakesMove={this.props.userMakesMove}
-            />
-            <Slot
-              value={this.props.board[7]}
-              userMakesMove={this.props.userMakesMove}
-            />
-            <Slot
-              value={this.props.board[8]}
-              userMakesMove={this.props.userMakesMove}
-            />
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+    render() {
+        const winning = this.props.complete;
+        return (
+            <React.Fragment>
+                <h1>{winning ? "Ya Won" : ""}</h1>
+                <button onClick={this.props.scramble}>Scramble</button>
+                <div className="border">
+                    <div className="row">
+                        <Slot
+                            value={this.props.board[0]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                        <Slot
+                            value={this.props.board[1]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                        <Slot
+                            value={this.props.board[2]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                    </div>
+                    <div className="row">
+                        <Slot
+                            value={this.props.board[3]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                        <Slot
+                            value={this.props.board[4]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                        <Slot
+                            value={this.props.board[5]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                    </div>
+                    <div className="row">
+                        <Slot
+                            value={this.props.board[6]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                        <Slot
+                            value={this.props.board[7]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                        <Slot
+                            value={this.props.board[8]}
+                            userMakesMove={this.props.userMakesMove}
+                        />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
